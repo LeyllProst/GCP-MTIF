2536ce5dbe046528b1b232dfc9b29aba597d52b1
Laszlo Levai <laszlo.lev.levai@gmail.com>
2024-11-01 11:09:00 +0000

    public and private subnets

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# VPC code
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -13,9 +14,20 @@ locals {
   zones = data.google_compute_zones.this.names
 }
 
+# NETWORK code
 resource "google_compute_network" "this" {
-  name                            = "${var.main_network_name}"
+  name                            = var.main_network_name
   delete_default_routes_on_create = false
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
+}
+
+# SUBNETS
+resource "google_compute_subnetwork" "this" {
+  count                    = 2
+  name                     = "${var.main_network_name}-${local.type[count.index]}-subnets"
+  ip_cidr_range            = var.ip_cidr_range[count.index]
+  region                   = var.region
+  network                  = google_compute_network.this.id
+  private_ip_google_access = true
 }
