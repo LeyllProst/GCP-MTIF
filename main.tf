32870e226a904614bef5dd348d7da8a9309486cb
Laszlo Levai <laszlo.lev.levai@gmail.com>
2024-11-01 11:51:07 +0000

    NAT for private network

--- main.tf
+++ main.tf
@@ -25,9 +25,29 @@ resource "google_compute_network" "this" {
 # SUBNETS
 resource "google_compute_subnetwork" "this" {
   count                    = 2
-  name                     = "${var.main_network_name}-${local.type[count.index]}-subnets"
+  name                     = "${var.main_network_name}-${local.type[count.index]}-subnetwork"
   ip_cidr_range            = var.ip_cidr_range[count.index]
   region                   = var.region
   network                  = google_compute_network.this.id
   private_ip_google_access = true
 }
+
+# NAT ROUTER
+resource "google_compute_router" "this" {
+  name    = "${var.main_network_name}-${local.type[1]}-router"
+  region  = google_compute_subnetwork.this[1].region
+  network = google_compute_network.this.id
+}
+
+resource "google_compute_router_nat" "this" {
+  name                               = "${var.main_network_name}-${local.type[1]}-router-nat"
+  router                             = google_compute_router.this.name
+  region                             = google_compute_router.this.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = "${var.main_network_name}-${local.type[1]}-subnetwork"
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
