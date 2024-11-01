d7f5496b04c0f33fdf2c455431237f3c0b0c6249
Laszlo Levai <laszlo.lev.levai@gmail.com>
2024-11-01 11:09:00 +0000

    public and private subnets

--- variables.tf
+++ variables.tf
@@ -15,3 +15,9 @@ variable "main_network_name" {
   description = "Name for backbone"
   default     = "backbone"
 }
+
+variable "ip_cidr_range" {
+  type        = list(string)
+  description = "List of The range of internal addresses that are owned by this subnetwork."
+  default     = ["10.10.10.0/24", "10.10.20.0/24"]
+}
