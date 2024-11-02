812d4db00c078cd4a82eb4ecd0ac49fe18717f5b
Laszlo Levai <laszlo.lev.levai@gmail.com>
2024-11-02 22:30:40 +0000

    module/instance and saltmaster instance code

---
+++ modules/instance/README.md
@@ -0,0 +1,19 @@
+# MODULES/INSTANCE
+
+## purpose
+
+to create virtual machine - google_compute_instance
+
+## variables
+
+- instance_name
+- instance_type
+- update_stopping
+- deletion_protection
+- network
+- sub_network
+- bootdisk_autodelete
+- bootdisk_image_size
+- image
+- labels
+- startup_script
---
+++ modules/instance/main.tf
@@ -0,0 +1,26 @@
+
+resource "google_compute_instance" "this" {
+  name = "${var.instance_name}"
+  zone = "${var.zone}"
+  machine_type = "${var.instance_type}"
+  allow_stopping_for_update = "${var.update_stopping}"
+  deletion_protection = "${var.deletion_protection}"
+  labels = var.labels
+
+  network_interface {
+    network = "${var.network}"
+    subnetwork = "${var.sub_network}"
+  }
+
+  boot_disk {
+    auto_delete = "${var.bootdisk_autodelete}"
+    initialize_params {
+      image = "${var.image}"
+      size = var.bootdisk_image_size
+    }
+  }
+
+  metadata = {
+     startup-script = "${var.startup_script}"
+   }
+}
