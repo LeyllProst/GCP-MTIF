bbcd30032ff85e9bf37ef15ad6f6432d6e3af93a
Laszlo Levai <laszlo.lev.levai@gmail.com>
2024-11-02 21:48:27 +0000

    code formatting

--- global/vpc/main.tf
+++ global/vpc/main.tf
@@ -3,6 +3,25 @@ provider "google" {
   region  = var.region
 }
 
+resource "google_storage_bucket" "terraform_state" {
+  name                        = "terraform-state-us-central1-mtif-439912"
+  location                    = "us-central1"
+  uniform_bucket_level_access = true
+  force_destroy               = true
+  storage_class               = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-us-central1-mtif-439912"
+    prefix = "terraform/state"
+  }
+}
+
 module "vpc" {
   source = "../../modules/vpc"
 
