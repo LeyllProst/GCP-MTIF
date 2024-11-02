5d00b92f01189d9e6078ce1a0fb9c0f202af9624
Laszlo Levai <laszlo.lev.levai@gmail.com>
2024-11-02 23:18:57 +0000

    install packages

--- prod/main.tf
+++ prod/main.tf
@@ -22,5 +22,5 @@ module "saltmaster" {
   labels = {
     "purpose" = "salt-master"
   }
-  startup_script = "dnf update -y && dnf install -y mc vim"
+  startup_script = "dnf update -y && dnf install -y mc vim net-tools"
 }
