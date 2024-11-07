38e226655e91d0bd81d3c0bc245905199e544718
LeyllProst <74118856+LeyllProst@users.noreply.github.com>
2024-11-07 20:52:01 +0100

    feat(Saltstack infrastructure): merge working code (#8)
    * feat(saltstack-infrastructure): saltmaster has epheremal IP
    
    * feat(saltstack-infrastructure): install git on saltmaster
    
    * feat(salt-infrastructure): add node-1
    
    * docs(README.md):  documentation
    
    * feat(salt-infrastructure): copy ssh key
    
    * feat(saltstack-infrastructure): remove dead code
    
    * feat(saltstack-infrastructure): node naming convention
    
    * feat(salt-infrastructure): extend startup script
    
    * feat(saltstack-infrastructure): add working code

--- prod/README.md
+++ prod/README.md
@@ -3,3 +3,7 @@
 ## SALTSTACK infrastructure
 
 everything encapsulated here for testing the saltstack automation
+
+Contains:
+- saltmaster node
+- node-1 node (Rocky Linux 9.x)
--- prod/saltstack-infrastructure/main.tf
+++ prod/saltstack-infrastructure/main.tf
@@ -16,8 +16,10 @@ module "saltmaster" {
   instance_type       = "e2-medium"
   update_stopping     = true
   deletion_protection = false
+
   network             = data.terraform_remote_state.vcp.outputs.main_network_name
   sub_network         = data.terraform_remote_state.vcp.outputs.subnetworks_name[1]
+  network_ip          = "10.10.20.5"
   bootdisk_image_size = 20
   image               = "projects/rocky-linux-cloud/global/images/rocky-linux-9-optimized-gcp-v20241009"
 
@@ -26,13 +28,92 @@ module "saltmaster" {
   }
 
   startup_script = <<EOF
-dnf update -y && dnf install -y mc vim net-tools bind-utils
+dnf update -y
 curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.repo | sudo tee /etc/yum.repos.d/salt.repo
 dnf install -y salt-master salt-minion salt-ssh salt-syndic salt-cloud salt-api
-systemctl enable salt-master && sudo systemctl start salt-master
-systemctl enable salt-minion && sudo systemctl start salt-minion
-systemctl enable salt-syndic && sudo systemctl start salt-syndic
-systemctl enable salt-api && sudo systemctl start salt-api
+systemctl enable salt-master
+systemctl enable salt-minion
+systemctl enable salt-syndic && systemctl start salt-syndic
+systemctl enable salt-api && systemctl start salt-api
+echo "master: 10.10.20.5" > /etc/salt/minion.d/minion.conf
+systemctl restart salt-minion
+
+cat << 'EOG' >> /etc/salt/master.d/master.conf
+## MASTER CONFIG parameters
+interface: 10.10.20.5
+ipv6: False
+state_verbose: False
+
+## ENVIRONMENT definitions
+default_top: production
+state_top_saltenv: production
+top_file_merging_strategy: same
+
+env_order:
+  - production
+  - maintenance
+
+file_roots:
+  production:
+    - /opt/infrastructure/production
+  maintenance:
+    - /opt/infrastructure/maintenance
+EOG
+
+cat << 'EOH' >> /etc/salt/minion.d/role_base.conf
+grains:
+  role:
+    - base
+EOH
+
+systemctl restart salt-master
 EOF
 }
+
+# salt minions
+module "salt-node-1" {
+  source              = "../../modules/instance"
+  instance_name       = "salt-node-1"
+  zone                = "us-central1-a"
+  instance_type       = "e2-small"
+  update_stopping     = true
+  deletion_protection = false
+
+  network             = data.terraform_remote_state.vcp.outputs.main_network_name
+  sub_network         = data.terraform_remote_state.vcp.outputs.subnetworks_name[1]
+  bootdisk_image_size = 20
+  image               = "projects/rocky-linux-cloud/global/images/rocky-linux-9-optimized-gcp-v20241009"
+
+  labels = {
+    "purpose" = "salt-minion"
+  }
+
+  startup_script = <<EOF
+dnf update -y
+curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.repo | sudo tee /etc/yum.repos.d/salt.repo
+dnf install -y salt-minion
+systemctl enable salt-minion
+echo "master: 10.10.20.5" > /etc/salt/minion.d/minion.conf
+cat << 'EOG' >> /etc/salt/minion.d/role_base.conf
+grains:
+  role:
+    - base
+EOG
+
+systemctl restart salt-minion
+EOF
+}
+
+# [root@saltmaster ~]# cat /etc/salt/minion.d/environment.conf 
+# saltenv: maintenance
+
+# [root@saltmaster ~]# cat /opt/infrastructure/production/top.sls 
+# production:
+#   '*':
+#     - core
+
+# [root@saltmaster ~]# cat /opt/infrastructure/maintenance/top.sls 
+# maintenance:
+#   '*':
+#     - core
 
