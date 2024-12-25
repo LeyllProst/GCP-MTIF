8ddc798841f446b7ef88a790a50a7c90414e73f0
LeyllProst <74118856+LeyllProst@users.noreply.github.com>
2024-12-25 22:22:29 +0100

    correcting fw name

--- prod/saltstack-infrastructure/main.tf
+++ prod/saltstack-infrastructure/main.tf
@@ -55,7 +55,7 @@ module "repository" {
 module "saltstack_firewall" {
   source = "git@github.com:LeyllProst/gcp-mtif-firewall.git?ref=v1.0.0"
 
-  name                = "saltstack firewall rules"
+  firewall_name                = "saltstack firewall rules"
   network             = data.terraform_remote_state.vcp.outputs.main_network_name
 
   allow_rules = [
