c069b4d54f2bdd4fb9b42e4334b11874a73c9f87
LeyllProst <74118856+LeyllProst@users.noreply.github.com>
2024-12-25 22:17:13 +0100

    add firewall rules

--- prod/saltstack-infrastructure/main.tf
+++ prod/saltstack-infrastructure/main.tf
@@ -50,3 +50,18 @@ module "repository" {
 
   startup_script = var.repository-startup_script
 }
+
+# FIREWALL rules
+module "saltstack_firewall" {
+  source = "git@github.com:LeyllProst/gcp-mtif-firewall.git?ref=v1.0.0"
+
+  name                = "saltstack firewall rules"
+  network             = data.terraform_remote_state.vcp.outputs.main_network_name
+
+  allow_rules = [
+    {
+      protocol = "tcp"
+      ports    = ["4505", "4506", "443"]
+    }
+  ]
+} 
