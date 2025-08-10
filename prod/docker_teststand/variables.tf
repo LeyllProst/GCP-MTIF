11f9f23a34ea5e0cfd6bab8e0fd02e6205e026bb
LeyllProst <74118856+LeyllProst@users.noreply.github.com>
2025-08-10 13:34:59 +0200

    fix variable naming for docker teststand

--- prod/docker_teststand/variables.tf
+++ prod/docker_teststand/variables.tf
@@ -1,63 +1,63 @@
 # DOCKER-TESTSTAND variables
-variable "docker-teststand-update_stopping" {
+variable "docker_teststand_update_stopping" {
   type    = bool
   default = true
 }
 
-variable "docker-teststand-deletion_protection" {
+variable "docker_teststand_deletion_protection" {
   type    = bool
   default = false
 }
 
-variable "docker-teststand-instance_name" {
+variable "docker_teststand_instance_name" {
   type    = string
   default = "docker-teststand"
 }
 
-variable "docker-teststand_instance_zone" {
+variable "docker_teststand_instance_zone" {
   type    = string
   default = "us-central1-f"
 }
 
-variable "docker-teststand-network_ip" {
+variable "docker_teststand_network_ip" {
   type    = string
   default = "10.10.10.5"
 }
 
-variable "docker-teststand-instance_type" {
+variable "docker_teststand_instance_type" {
   type    = string
   default = "e2-standard-4"
 }
 
-variable "docker-teststand-instance_labels" {
+variable "docker_teststand_instance_labels" {
   type = map(string)
   default = {
     "purpose" = "docker"
   }
 }
 
-variable "docker-teststand-bootdisk_image_size" {
+variable "docker_teststand_bootdisk_image_size" {
   type    = number
   default = 30
 }
 
-variable "docker-teststand-image" {
+variable "docker_teststand_image" {
   type    = string
   default = "projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2504-plucky-amd64-v20250430"
 }
 
-variable "docker-teststand-assign_ephemeral_external_ip" {
+variable "docker_teststand_assign_ephemeral_external_ip" {
   type    = bool
   default = true
 }
 
-variable "docker-teststand-startup_script" {
+variable "docker_teststand_startup_script" {
   type        = string
   description = "commands for startup"
   default     = <<-EOF
     #!/bin/bash
     set -e
-    
+
     apt update && apt upgrade -y
     apt install docker-compose-v2 vim htop mc -y
   EOF
