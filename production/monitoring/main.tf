46c7f66e646016d643c383e5170369ddec5535ee
Laszlo <laszlo.lev.levai@gmail.com>
2025-09-14 11:29:34 +0200

    feat(monitoring): configure own and remote state backend

--- production/monitoring/main.tf
+++ production/monitoring/main.tf
@@ -1,9 +1,17 @@
+# Remote State backend configuration
+terraform {
+  backend "gcs" {
+    bucket = "terraform_remote_state_us-central1_mtif-439912"
+    prefix = "terraform/monitoring"
+  }
+}
+
 # REMOTE STATE reading from gcs
-data "terraform_remote_state" "vpc" {
+data "terraform_remote_state" "trs" {
   backend = "gcs"
 
   config = {
-    bucket = "terraform-state-us-central1-mtif-439912"
+    bucket = "terraform_remote_state_us-central1_mtif-439912"
     prefix = "terraform/remote-state"
   }
 }
@@ -13,7 +21,7 @@ data "terraform_remote_state" "vpc" {
 module "monitoring" {
   source = "git@github.com:LeyllProst/gcp-mtif-instances.git?ref=v2.1.2"
 
-  project_id = data.terraform_remote_state.vpc.output.project
+  project_id = data.terraform_remote_state.trs.outputs.project_id
   network    = data.terraform_remote_state.vpc.outputs.vpc_network_self_link
   subnetwork = data.terraform_remote_state.vpc.outputs.vpc_subnetwork_self_link[0]
 
