4111ccaeeafdb799113b4e3fe55e87f5f1d6cb1c
LeyllProst <74118856+LeyllProst@users.noreply.github.com>
2025-09-15 18:55:39 +0200

    feat(saltstack-infrastructure): refactoring (#25)

--- production/saltstack-infrastructure/main.tf
+++ production/saltstack-infrastructure/main.tf
@@ -1,13 +1,29 @@
-# REMOTE STATE reading from gcs
-data "terraform_remote_state" "vcp" {
-  backend = "gcs"
-
-  config = {
-    bucket = "terraform-state-us-central1-mtif-439912"
-    prefix = "terraform/state"
+# Remote State backend configuration
+terraform {
+  backend "gcs" {
+    bucket = "terraform_remote_state_us-central1_mtif-439912"
+    prefix = "terraform/saltstack-infrastructure"
   }
 }
 
+# REMOTE STATE reading from gcs
+data "terraform_remote_state" "trs" {
+  backend = "gcs"
+
+  config = {
+    bucket = "terraform_remote_state_us-central1_mtif-439912"
+    prefix = "terraform/remote-state"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "gcs"
+
+  config = {
+    bucket = "terraform_remote_state_us-central1_mtif-439912"
+    prefix = "terraform/vpc"
+  }
+}
 
 
 
@@ -15,9 +31,9 @@ data "terraform_remote_state" "vcp" {
 module "saltmaster" {
   source = "git@github.com:LeyllProst/gcp-mtif-instances.git?ref=v2.0.2"
 
-  project_id          = data.terraform_remote_state.vcp.outputs.project
-  network             = data.terraform_remote_state.vcp.outputs.vpc_network_self_link
-  subnetwork          = data.terraform_remote_state.vcp.outputs.vpc_subnetwork_self_link[1]
+  project_id          = data.terraform_remote_state.trs.outputs.project_id
+  network    = data.terraform_remote_state.vpc.outputs.vpc_network_self_link
+  subnetwork = data.terraform_remote_state.vpc.outputs.vpc_subnetwork_self_link[1]
   instance_name       = var.saltmaster-instance_name
   zone                = var.saltmaster-instance_zone
   network_ip          = var.saltmaster-network_ip
@@ -39,9 +55,9 @@ module "saltmaster" {
 module "repository" {
   source = "git@github.com:LeyllProst/gcp-mtif-instances.git?ref=v2.0.2"
 
-  project_id          = data.terraform_remote_state.vcp.outputs.project
-  network             = data.terraform_remote_state.vcp.outputs.vpc_network_self_link
-  subnetwork          = data.terraform_remote_state.vcp.outputs.vpc_subnetwork_self_link[1]
+  project_id          = data.terraform_remote_state.trs.outputs.project_id
+  network    = data.terraform_remote_state.vpc.outputs.vpc_network_self_link
+  subnetwork = data.terraform_remote_state.vpc.outputs.vpc_subnetwork_self_link[1]
   instance_name       = var.repository-instance_name
   zone                = var.repository-instance_zone
   instance_type       = var.repository-instance_type
@@ -59,9 +75,9 @@ module "repository" {
 module "saltstack_firewall" {
   source = "git@github.com:LeyllProst/gcp-mtif-firewall.git?ref=v2.1.0"
 
-  project       = data.terraform_remote_state.vcp.outputs.project
+  project          = data.terraform_remote_state.trs.outputs.project_id
   firewall_name = "saltstack-firewall-rules"
-  network       = data.terraform_remote_state.vcp.outputs.vpc_network_self_link
+  network       = data.terraform_remote_state.vpc.outputs.vpc_network_self_link
 
   source_ranges = ["0.0.0.0/0"]
 
